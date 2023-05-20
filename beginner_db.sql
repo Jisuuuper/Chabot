8435802955b4055bba08c699637aaf6811edccae
JZK08 <74087445+JZK08@users.noreply.github.com>
2023-05-19 19:06:44 -1200

    new database

--- beginner_db.sql
+++ beginner_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 15, 2023 at 05:06 PM
+-- Generation Time: May 20, 2023 at 09:05 AM
 -- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,26 @@ SET time_zone = "+00:00";
 --
 -- Database: `beginner_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chatbot`
+--
+
+CREATE TABLE `chatbot` (
+  `id` int(11) NOT NULL,
+  `responses` varchar(255) NOT NULL,
+  `patterns` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `chatbot`
+--
+
+INSERT INTO `chatbot` (`id`, `responses`, `patterns`) VALUES
+(1, 'hihih', 'HowtocodeJava?'),
+(2, 'To get Java library, get somethingblabla', 'How to get Java library?');
 
 -- --------------------------------------------------------
 
@@ -72,7 +92,9 @@ INSERT INTO `posts` (`id`, `user_id`, `title_post`, `post`, `image`, `tag`, `dat
 (88, 23, 'firts post', 'fisrtpost', '', 'PYTHON', '2023-05-15 17:00:22'),
 (89, 23, 'second post', 'second post', '', 'PYTHON', '2023-05-15 17:00:35'),
 (90, 23, 'second post', 'second post', '', 'PYTHON', '2023-05-15 17:03:34'),
-(91, 23, 'third post', 'third post', '', 'PYTHON', '2023-05-15 17:03:52');
+(91, 23, 'third post', 'third post', '', 'PYTHON', '2023-05-15 17:03:52'),
+(92, 31, 'HowtocodeJava?', 'haha', '', 'JAVA', '2023-05-20 07:53:41'),
+(93, 31, 'How to get Java library?', 'hihihihhjava', '', 'JAVA', '2023-05-20 08:59:56');
 
 -- --------------------------------------------------------
 
@@ -97,11 +119,19 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `date`, `image`, `user_type`) VALUES
 (23, 'Jireh', 'jirehaguraso@gmail.com', '1234', '2023-05-13 04:39:13', 'uploads/OIP (1).jfif', 'user'),
 (24, 'loyd', 'loyd@gmail.com', '1234', '2023-05-13 04:40:36', NULL, 'user'),
-(25, 'Albert', 'albert@gmail.com', '1234', '2023-05-13 05:52:41', NULL, 'user');
+(25, 'Albert', 'albert@gmail.com', '1234', '2023-05-13 05:52:41', NULL, 'user'),
+(31, 'user', 'user@usls.edu.ph', '123', '2023-05-20 07:46:58', NULL, 'user'),
+(32, 'admin', 'admin@usls.edu.ph', '123', '2023-05-20 07:55:20', NULL, 'admin');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `chatbot`
+--
+ALTER TABLE `chatbot`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `forum`
@@ -131,6 +161,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `chatbot`
+--
+ALTER TABLE `chatbot`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `forum`
 --
 ALTER TABLE `forum`
@@ -140,13 +176,13 @@ ALTER TABLE `forum`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=92;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
